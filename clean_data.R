56f613ab0fed2c0f9309fb80ec10c2300e3f15b2
Noah Johnson <noah.johnson13@ncf.edu>
2018-04-29 18:27:22 -0400

    Working on data subsetting in clean_data script

--- clean_data.R
+++ clean_data.R
@@ -1,9 +1,14 @@
-load(file = "data/realitymining.Rda")
+###
+# EDA Things to subset the data
+###
 
-n <- 94
+load(file = "/home/noah/git/datavis_project2/data/realitymining.Rda")
 
-groups <- rep(NA, n)
-for (subjectID in 1:n) {
+N <- 94
+
+# Save my.group response for every subject
+groups <- rep(NA, N)
+for (subjectID in 1:N) {
   
   temp <- data$s["my.group", 1, subjectID][[1]]
   
@@ -19,3 +24,24 @@ for (subjectID in 1:n) {
 }
 
 save(groups, file = "/home/noah/git/datavis_project2/data/groups.Rda")
+
+# Save survey start date for every subject
+start.dates <- rep(NA, N)
+for (subjectID in 1:N) {
+  
+  temp <- data$s["survey.start.n", 1, subjectID][[1]]
+  
+  if (ncol(temp) > 0) {
+    start.dates[subjectID] <- temp[1,1]
+  }
+}
+
+save(start.dates, file = "/home/noah/git/datavis_project2/data/startDates.Rda")
+
+# Save text message response for every subject
+
+# Save friends matrix
+friends <- data$network[[1]]
+
+save(friends, file = "/home/noah/git/datavis_project2/data/friendsmatrix.Rda")
+
