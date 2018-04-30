5e3d8089102e1ada08a740d83d0704319d9e2ffe
Noah R. Johnson <noah.johnson13@ncf.edu>
2018-04-30 14:30:34 -0400

    working to get network indices mapped to proper subject IDs

--- clean_data.R
+++ clean_data.R
@@ -4,12 +4,24 @@
 
 load(file = "/home/noah/git/datavis_project2/data/realitymining.Rda")
 
-N <- 94
+# Save friends matrix and mapping of indices to subject IDs
+friends <- data$network[[1]]
 
-# Save my.group response for every subject
+N <- nrow(friends)
+
+sub_sort <- data$network[4][[1]][1,]
+
+mapped_ids <- sapply(1:94, function(x) return (sub_sort[x]))
+
+save(friends, file = "/home/noah/git/datavis_project2/data/friendsmatrix.Rda")
+
+save(mapped_ids, file = "/home/noah/git/datavis_project2/data/networkIDmapping.Rda")
+
+
+# Save my.group response for every subject in network
 groups <- rep(NA, N)
-for (subjectID in 1:N) {
-  
+for (subjectID in mapped_ids) {
+  print(subjectID)
   temp <- data$s["my.group", 1, subjectID][[1]]
   
   if (!is_empty(temp)) {
@@ -40,8 +52,22 @@ save(start.dates, file = "/home/noah/git/datavis_project2/data/startDates.Rda")
 
 # Save text message response for every subject
 
-# Save friends matrix
-friends <- data$network[[1]]
 
-save(friends, file = "/home/noah/git/datavis_project2/data/friendsmatrix.Rda")
+# Get a list of mac addresses in integer form for every subject
+library(install.load)
+install_load('Rmpfr')
 
+macs <- rep(NA, N)
+for (subjectID in 1:N) {
+  
+  temp <- data$s["my.mac", 1, subjectID][[1]]
+  #print(subjectID)
+  if (length(temp) != 0) {
+    macs[subjectID] <- as.numeric(mpfr(temp[[1]][[1]][1,1], base=16))
+  }
+}
+
+# Save proximity events
+proximity.events <- rep(NA, N)
+
+save(groups, file = "/home/noah/git/datavis_project2/data/groups.Rda")
