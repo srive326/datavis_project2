daf6403c4b2ed561787e3ae730247645f9f3d74a
Noah R. Johnson <noah.johnson13@ncf.edu>
2018-04-30 18:54:59 -0400

    Got network graph in shiny app under friendship tab

--- Self-Report-Bias/app.R
+++ Self-Report-Bias/app.R
@@ -10,57 +10,67 @@
 library(shiny)
 library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(theme = "app.css",
-  
-   # Application title
-   titlePanel("Visualizing Survey Bias"),
-   
-   # 
-   actionButton("predictabilityButton", "Predictability"),
-   actionButton("friendshipButton", "Friendship"),
-   actionButton("communicationButton", "Communication"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+# Load data for vizzes
+load(file = "data/friendsmatrix.Rda")
+load(file = "data/networkIDmapping.Rda")
+load(file = "data/groups.Rda")
+load(file = "data/proximityEvents.Rda")
 
-# Define server logic required to draw a histogram
+
+#####################################################
+# Create nodes and edges for friendship network graph
+n <- nrow(friends)
+
+sources <- c()
+targets <- c()
+values <- c()
+
+# Generate links
+k <- 1
+for (i in 1:N) {
+  for (j in 1:N) {
+    if (!is.na(friends[i,j])) {
+      sources[k] <- i-1 # forceNetwork requires 0 indexing
+      targets[k] <- j-1
+      values[k] <- log(proximity.events[i,j] + proximity.events[j,i] + 1) + 1 # pad by 1 so there's no zero-width links
+      k <- k + 1
+    }
+  }
+}
+
+# Count number of friends for every subject in the network
+sizes <- sapply(1:N, function(s) return (exp(sum(targets + 1 == s))), USE.NAMES = FALSE)
+
+# Create df for nodes (1 per subject)
+friendNodes <- data.frame(subjectID = mapped_ids, size = sizes, group = groups)
+
+# Create df for links
+friendLinks <- data.frame(source = sources, target = targets, value = values)
+
+#####################################################
+
+
+# Define UI with tabs
+ui <- navbarPage("Visualizing Survey Bias",
+                 tabPanel("Predictability", plotOutput("distPlot")),
+                 tabPanel("Friendship", forceNetworkOutput("friendNetwork"))
+      )
+
+
+# Define server logic required to draw vizzes
 server <- function(input, output) {
-  
-  observeEvent(input$predictabilityButton, {
-    # block of code run on button press
-    print('hi predictable')
-  })
-  observeEvent(input$friendshipButton, {
-    # block of code run on button press
-    print('hi friend')
-  })
-  observeEvent(input$communicationButton, {
-    # block of code run on button press
-    print('hi communication')
-  })
    
    output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+     plot(iris)
+   })
+   
+   output$friendNetwork <- renderForceNetwork({
+     forceNetwork(Links = friendLinks, Nodes = friendNodes, Source = "source", 
+                  Target = "target", Value = "value", NodeID = "subjectID", 
+                  Nodesize = "size", Group = "group", 
+                  linkWidth = JS("function(d) { return Math.sqrt(d.value); }"), 
+                  arrows = TRUE, legend = TRUE, zoom = T, 
+                  opacity = 1, opacityNoHover = 1, charge = -75)
    })
 }
 
